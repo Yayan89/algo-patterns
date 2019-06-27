06c2b366c3c3fdcf42a3e26a74586c10a25bc6c8
yandatson@gmail.com <yandatson@gmail.com>
2019-06-27 16:28:02 -0400

    added bubble sort practice

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -1,5 +1,28 @@
 package sort;
 
+import java.util.Arrays;
+
 public class BubbleSort {
-w
+    // [20,-15,7,35,1,-22,55]
+    public static void main(String[] args) {
+        int[] array = {20, 35, -15, 7, 1, 55, -22};
+        sort(array);
+    }
+
+    private static void sort(int[] array) {
+
+        //1. get the whole array
+        for (int i = array.length - 1; i > 0; i--) {
+            //for each element in array
+            for (int k = 0; k < i; k++) {
+                if (array[k] > array[k + 1]) {
+                    int holder = array[k]; //20
+                    array[k] = array[k+1]; //
+                    array[k+1] = holder;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
 }

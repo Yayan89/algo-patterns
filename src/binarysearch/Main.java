def57376ab772d6c89669699ffd5684e557bc250
Dat <yandatson@gmail.com>
2019-10-23 14:12:44 -0400

    cleaned BinarySearch
    cleaned recursion method

--- src/binarysearch/Main.java
+++ src/binarysearch/Main.java
@@ -1,30 +1,35 @@
 package binarysearch;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
         int[] intArray = { -22, -15, 1 ,7 ,20, 35, 55 };
         System.out.println(recursiveBinarySearch(intArray, 55));
+        System.out.println(recursiveBinarySearch(intArray, -22));
+        System.out.println(recursiveBinarySearch(intArray, 7));
+        System.out.println(recursiveBinarySearch(intArray, 60));
     }
 
     private static int recursiveBinarySearch(int[] input, int value) {
         int mid = input.length / 2;
         int end = input.length;
-        int[] newArray = new int[input.length];
 
-        if(newArray.length == 0) {
+        //if it does not exist
+        if(input.length == 0) {
             return -1;
+        }
+        //if it exist
+        if(input[mid] == value) {
+            return 1;
         }
 
         if(input[mid] < value) {
-            newArray = Arrays.copyOfRange(input, mid + 1, end);
-        } else if (input[mid] > value){
-            newArray = Arrays.copyOfRange(input, 0, mid);
-        } else if(input[mid] == value) {
-            return 1;
+            input = Arrays.copyOfRange(input, mid + 1, end);
+            return recursiveBinarySearch(input, value);
+        } else {
+            input = Arrays.copyOfRange(input, 0, mid);
+            return recursiveBinarySearch(input, value);
         }
-        return recursiveBinarySearch(newArray, value);
     }
 }

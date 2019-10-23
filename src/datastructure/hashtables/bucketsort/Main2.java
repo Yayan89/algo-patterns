ddb40750e4337e33754a27f3dfe82a758af62eda
Dat <yandatson@gmail.com>
2019-10-23 10:59:36 -0400

    added second way
    with for loop instead of Iterator

--- src/datastructure/hashtables/bucketsort/Main2.java
+++ src/datastructure/hashtables/bucketsort/Main2.java
@@ -4,6 +4,7 @@ import datastructure.Employee;
 
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.stream.IntStream;
 
 public class Main2 {
     public static void main(String[] args) {
@@ -16,16 +17,27 @@ public class Main2 {
         list.add(new Employee("Jane", "Jones", 123));
 
         HashMap<Integer, Employee> hashMap = new HashMap<>();
-        Iterator<Employee> iterator = list.iterator();
+//        Iterator<Employee> iterator = list.iterator();
         LinkedList<Employee> remove = new LinkedList<>();
 
-        while(iterator.hasNext()) {
-            Employee next = iterator.next();
+//        while(iterator.hasNext()) {
+//            Employee next = iterator.next();
+//
+//            if(hashMap.containsKey(next.getId())) {
+//                remove.add(next);
+//            } else {
+//                hashMap.put(next.getId(), next);
+//            }
+//        }
 
-            if(hashMap.containsKey(next.getId())) {
-                remove.add(next);
+        //second way
+        int bound = list.size();
+        for (int i = 0; i < bound; i++) {
+            Employee currentEmp = list.get(i);
+            if (hashMap.containsValue(currentEmp)) {
+                remove.add(currentEmp);
             } else {
-                hashMap.put(next.getId(), next);
+                hashMap.put(i, currentEmp);
             }
         }
 

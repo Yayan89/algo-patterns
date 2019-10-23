c5e352aae5506baf225a03d5443e3d04b7de84e5
Dat <yandatson@gmail.com>
2019-10-23 11:04:42 -0400

    replace Iterator with foreach loop
    more concise way

--- src/datastructure/hashtables/bucketsort/Main2.java
+++ src/datastructure/hashtables/bucketsort/Main2.java
@@ -17,29 +17,27 @@ public class Main2 {
         list.add(new Employee("Jane", "Jones", 123));
 
         HashMap<Integer, Employee> hashMap = new HashMap<>();
-//        Iterator<Employee> iterator = list.iterator();
         LinkedList<Employee> remove = new LinkedList<>();
 
-//        while(iterator.hasNext()) {
-//            Employee next = iterator.next();
-//
-//            if(hashMap.containsKey(next.getId())) {
-//                remove.add(next);
-//            } else {
-//                hashMap.put(next.getId(), next);
-//            }
-//        }
-
-        //second way
-        int bound = list.size();
-        for (int i = 0; i < bound; i++) {
-            Employee currentEmp = list.get(i);
-            if (hashMap.containsValue(currentEmp)) {
-                remove.add(currentEmp);
+        //more clear and concise way
+        for (Employee next : list) {
+            if (hashMap.containsKey(next.getId())) {
+                remove.add(next);
             } else {
-                hashMap.put(i, currentEmp);
+                hashMap.put(next.getId(), next);
             }
         }
+
+        //second way
+//        int bound = list.size();
+//        for (int i = 0; i < bound; i++) {
+//            Employee currentEmp = list.get(i);
+//            if (hashMap.containsValue(currentEmp)) {
+//                remove.add(currentEmp);
+//            } else {
+//                hashMap.put(i, currentEmp);
+//            }
+//        }
 
         remove.forEach(list::remove);
         System.out.println(list);

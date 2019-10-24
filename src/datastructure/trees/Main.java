a3b1c974be76cf5d3bdae1efb76c1c996d08f873
Dat <yandatson@gmail.com>
2019-10-24 13:35:56 -0400

    Added TreeNode
    added TreeNode implementation

--- src/datastructure/trees/Main.java
+++ src/datastructure/trees/Main.java
@@ -2,6 +2,18 @@ package datastructure.trees;
 
 public class Main {
     public static void main(String[] args) {
+        Tree intTree = new Tree();
+        intTree.insert(25);
+        intTree.insert(20);
+        intTree.insert(30);
+        intTree.insert(10);
+        intTree.insert(60);
+        intTree.insert(70);
+        intTree.insert(55);
+        intTree.insert(45);
+        intTree.insert(32);
 
+        intTree.printAll();
     }
+
 }

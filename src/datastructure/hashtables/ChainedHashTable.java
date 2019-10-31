efa0298ee4218e931739c58afc1dbd968a80df2a
Dak <yandatson@gmail.com>
2019-10-31 09:58:03 -0400

    Added ChainHash
    Much simpler implementation than Linear probing since we are using LinkedList instead of arrays which manage our collision for us.

---
+++ src/datastructure/hashtables/ChainedHashTable.java
@@ -0,0 +1,59 @@
+package datastructure.hashtables;
+
+import datastructure.Employee;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class ChainedHashTable {
+    @SuppressWarnings("unchecked")
+    private LinkedList<StoredEmployee>[] hashtable = new LinkedList[10];
+
+    public ChainedHashTable() {
+        Arrays.fill(hashtable, new LinkedList<>());
+    }
+
+    public void put(String key, Employee employee) {
+        int hashedKey = hashKey(key);
+        hashtable[hashedKey].add(new StoredEmployee(key, employee));
+    }
+
+    public Employee get(String key) {
+        int hashedKey = hashKey(key);
+        ListIterator<StoredEmployee> iterator = hashtable[hashedKey].listIterator();
+        StoredEmployee storedEmployee;
+
+        while (iterator.hasNext()) {
+            storedEmployee = iterator.next();
+            if(storedEmployee.getKey().equals(key)) {
+                return storedEmployee.getEmployee();
+            }
+        }
+        return null;
+    }
+
+    public Employee remove(String key) {
+        int hashedKey = hashKey(key);
+        ListIterator<StoredEmployee> iterator = hashtable[hashedKey].listIterator();
+        StoredEmployee storedEmployee = null;
+
+        while (iterator.hasNext()) {
+            storedEmployee = iterator.next();
+            if(storedEmployee.getKey().equals(key)) {
+                break;
+            }
+        }
+
+        if(storedEmployee == null) {
+            return null;
+        } else {
+            hashtable[hashedKey].remove(storedEmployee);
+            return storedEmployee.getEmployee();
+        }
+    }
+
+    private int hashKey(String key) {
+        return key.length() % hashtable.length;
+    }
+}
--- src/datastructure/hashtables/MainSimpleHash.java
+++ src/datastructure/hashtables/MainSimpleHash.java
@@ -6,14 +6,15 @@ public class MainSimpleHash {
     public static void main(String[] args) {
         SimpleHashTable hashTable = new SimpleHashTable();
         hashTable.put("test0", new Employee("young", "naive", 18));
-        hashTable.put("test01", new Employee("test", "HK", 120));
+        hashTable.put("test01", new Employee("test1", "HK", 120));
         hashTable.put("test002", new Employee("test2", "HK", 11));
         hashTable.put("test0003", new Employee("test3", "HK", 12));
         hashTable.put("test00004", new Employee("test4", "HK", 12));
         hashTable.put("test1", new Employee("test5", "HK", 12));
-        hashTable.printHashTable();
 
+        hashTable.remove("test002");
+        hashTable.remove("test0003");
 
-        System.out.println(hashTable.get("test1"));
+        System.out.println(hashTable.get("test0003"));
     }
 }

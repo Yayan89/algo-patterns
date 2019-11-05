a2f7488519dc1a8d8269d04f308425312aaef0f2
Dak <yandatson@gmail.com>
2019-11-05 15:51:57 -0500

    another cheery pick
    cheerrrrry

--- src/datastructure/DoSomething.java
+++ src/datastructure/DoSomething.java
@@ -4,5 +4,6 @@ public class DoSomething {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("Cerrypick me");
+        System.out.println("Cerrypick anoter time me");
     }
 }

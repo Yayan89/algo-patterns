aca883130e3a2322739cc3a30a476d9f0affa7dd
Dak <yandatson@gmail.com>
2019-11-05 15:51:08 -0500

    cheery pick time
    cherrrry

--- src/datastructure/DoSomething.java
+++ src/datastructure/DoSomething.java
@@ -3,5 +3,6 @@ package datastructure;
 public class DoSomething {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("Cerrypick me");
     }
 }

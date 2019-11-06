a9f6c005ef8bd63beb0b4ce4dca1a55f3335b12f
Dak <yandatson@gmail.com>
2019-11-06 09:40:07 -0500

    我们的爱

--- src/datastructure/DoSomething.java
+++ src/datastructure/DoSomething.java
@@ -8,5 +8,6 @@ public class DoSomething {
         System.out.println("from branch 1");
         System.out.println("squash this 1");
         System.out.println("squash this 2");
+        System.out.println("is this going to replaced by stash?");
     }
 }

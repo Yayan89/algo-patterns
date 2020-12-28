1d6b5039efcfa12127e69d35ca04eff4ea53f621
yayan89 <yandatson@gmail.com>
2020-12-28 11:47:20 -0500

    Completed O(N^3) ThreeNumberSum

--- src/arrays/threenumbersum/Main.java
+++ src/arrays/threenumbersum/Main.java
@@ -2,7 +2,10 @@ package arrays.threenumbersum;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,33 +19,29 @@ public class Main {
         targetSum = 6;
         integers = threeNumberSum(array, targetSum);
         integers.forEach(integers1 -> System.out.println(Arrays.toString(integers1)));
+//
+//        array = new int[] {1, 2, 3};
+//        targetSum = 7;
+//        integers = threeNumberSum(array, targetSum);
+//        integers.forEach(integers1 -> System.out.println(Arrays.toString(integers1)));
 
-        array = new int[] {1, 2, 3};
-        targetSum = 7;
-        integers = threeNumberSum(array, targetSum);
-        integers.forEach(integers1 -> System.out.println(Arrays.toString(integers1)));
+//        array = new int[] {8, 10, -2, 49, 14};
+//        targetSum = 57;
+//        List<Integer[]> integers = threeNumberSum(array, targetSum);
+//        integers.forEach(integers1 -> System.out.println(Arrays.toString(integers1)));
     }
 
     public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
         //Time O(Nlog(N))
         Arrays.sort(array);
-        //-8, -6, 1, 2, 3, 5, 6, 12
 
         //Time O(N^3) 3x for-loops
         List<Integer[]> result = new ArrayList<>();
         for (int i = 0; i < array.length - 1; i++) {
-            for (int k = i + 1; k < array.length - 1; k++) {
-
-                if(array.length == 3) {
-                    for (int z = k + 1; z <= array.length - 1; z++) {
-                        if (array[i] + array[k] + array[z] == targetSum) {
-                            result.add(new Integer[] {array[i], array[k], array[z]});
-                        }
-                    }
-                }
-                for (int z = k + 1; z < array.length - 1; z++) {
-                    if (array[i] + array[k] + array[z] == targetSum) {
-                        result.add(new Integer[] {array[i], array[k], array[z]});
+            for (int j = i + 1; j < array.length - 1; j++) {
+                for (int z = j + 1; z <= array.length - 1; z++) {
+                    if (array[i] + array[j] + array[z] == targetSum) {
+                        result.add(new Integer[] {array[i], array[j], array[z]});
                     }
                 }
             }

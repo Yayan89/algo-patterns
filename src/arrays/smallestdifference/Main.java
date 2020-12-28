6891cb20af960e8749005112b82efa8f43f0a35c
yayan89 <yandatson@gmail.com>
2020-12-28 16:16:03 -0500

    Working on better solution for smallestdifference

--- src/arrays/smallestdifference/Main.java
+++ src/arrays/smallestdifference/Main.java
@@ -7,13 +7,14 @@ public class Main {
         int[] arrayOne = {-1, 5, 10, 20, 28, 3};
         int[] arrayTwo = {26, 134, 135, 15, 17};
         //example [28,26] smallest difference
+        System.out.println(Arrays.toString(smallestDifference1(arrayOne, arrayTwo)));
 
 //        System.out.println(Arrays.toString(smallestDifference(arrayOne, arrayTwo)));
 
-        arrayOne = new int[] {10, 1000, 9124, 2142, 59, 24, 596, 591, 124, -123};
-        arrayTwo = new int[] {-1441, -124, -25, 1014, 1500, 660, 410, 245, 530};
-        //[-123,-124]
-        System.out.println(Arrays.toString(smallestDifference(arrayOne, arrayTwo)));
+//        arrayOne = new int[] {10, 1000, 9124, 2142, 59, 24, 596, 591, 124, -123};
+//        arrayTwo = new int[] {-1441, -124, -25, 1014, 1500, 660, 410, 245, 530};
+//        //[-123,-124]
+//        System.out.println(Arrays.toString(smallestDifference1(arrayOne, arrayTwo)));
 
     }
 
@@ -21,6 +22,8 @@ public class Main {
         int smallestDiff = Math.abs(Math.abs(arrayOne[0]) - Math.abs(arrayTwo[0]));
         int[] results = new int[2];
 
+        //O(N^2) T
+        //O(1) S
         for (int j : arrayOne) {
             for (int i : arrayTwo) {
                 int currentDiff = Math.abs(Math.abs(j) - Math.abs(i));
@@ -32,5 +35,30 @@ public class Main {
             }
         }
         return results;
+    }
+
+    public static int[] smallestDifference1(int[] arrayOne, int[] arrayTwo) {
+        //what happens if we sort?
+//        {-1, 3, 5, 10, 20, 28}
+//        {15, 17, 26, 134, 135}
+        [28, 26]
+
+        Arrays.sort(arrayOne);
+        Arrays.sort(arrayTwo);
+
+        int arrayOneIndex = 0;
+        int arrayTwoIndex = 0;
+
+        while (arrayOne[arrayOneIndex] < arrayTwo[arrayTwoIndex]) {
+            arrayOneIndex++;
+        }
+
+        while (arrayOne[arrayOneIndex] > arrayTwo[arrayTwoIndex]) {
+            arrayTwoIndex++;
+
+            if(arrayOne[arrayOneIndex + 1] +)
+        }
+
+        return new int[] {arrayOne[arrayOneIndex], arrayTwo[arrayTwoIndex]};
     }
 }

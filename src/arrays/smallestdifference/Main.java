5673fbc8911cb430b860e049a54c9286d2298a53
yayan89 <yandatson@gmail.com>
2020-12-28 14:17:20 -0500

    Added smallestdifference problem

---
+++ src/arrays/smallestdifference/Main.java
@@ -0,0 +1,13 @@
+package arrays.smallestdifference;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arrayOne = {-1, 5, 10, 20, 28, 3};
+        int[] arrayTwo = {26, 134, 135, 15, 17};
+    }
+
+    public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
+        // Write your code here.
+        return new int[] {};
+    }
+}

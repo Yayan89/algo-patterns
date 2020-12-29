cd41ca92acfa33a3148e92cd26cc7b5dff93d0f0
yayan89 <yandatson@gmail.com>
2020-12-29 13:01:20 -0500

    Add monotinicarray

---
+++ src/arrays/monotonicarray/Main.java
@@ -0,0 +1,13 @@
+package arrays.monotonicarray;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {-1, -5, -10, -1100, -1100, -1101, -1102, -9001};
+        System.out.println(isMonotonic(array));
+    }
+
+    public static boolean isMonotonic(int[] array) {
+        // Write your code here.
+        return false;
+    }
+}

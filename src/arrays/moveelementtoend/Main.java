840d9bed26c4b007e61096b115c13d6adcfb513e
yayan89 <yandatson@gmail.com>
2020-12-29 11:06:12 -0500

    Added move element to end

---
+++ src/arrays/moveelementtoend/Main.java
@@ -0,0 +1,15 @@
+package arrays.moveelementtoend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+        // Write your code here.
+        return new ArrayList<Integer>();
+    }
+}

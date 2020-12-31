982e26d799770d68fd60c0e5ca4d10f7db8f7f0a
yayan89 <yandatson@gmail.com>
2020-12-31 14:23:51 -0500

    working in spiral traverse

--- src/arrays/spiraltraverse/Main.java
+++ src/arrays/spiraltraverse/Main.java
@@ -8,11 +8,25 @@ public class Main {
         int[][] array = {
                 {1, 2, 3, 4},
                 {12, 13, 14, 5},
-                {11, 16, 15, 6},
+                {11, 16, 15, 6, 10},
                 {10, 9, 8, 7}};
+
+        System.out.println(spiralTraverse(array));
     }
 
     public static List<Integer> spiralTraverse(int[][] array) {
-        return new ArrayList<Integer>();
+        int firstPointer = 0;
+        int secondPointer = array[0].length - 1;
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < array.length; i++) {
+            list.add(array[firstPointer][i]);
+        }
+
+        for(int k = secondPointer; secondPointer > 0; secondPointer--) {
+
+        }
+
+        return new ArrayList<>();
     }
 }

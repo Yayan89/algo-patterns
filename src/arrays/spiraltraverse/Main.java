72e03b82c71caf9388d0a1e150d629b1530d29fc
yayan89 <yandatson@gmail.com>
2020-12-31 13:25:32 -0500

    Added test

--- src/arrays/spiraltraverse/Main.java
+++ src/arrays/spiraltraverse/Main.java
@@ -5,11 +5,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        int[][] array = {
+                {1, 2, 3, 4},
+                {12, 13, 14, 5},
+                {11, 16, 15, 6},
+                {10, 9, 8, 7}};
     }
 
     public static List<Integer> spiralTraverse(int[][] array) {
-        // Write your code here.
         return new ArrayList<Integer>();
     }
 }

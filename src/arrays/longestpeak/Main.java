796b0e9d492f4a8abd811b8db65dd6c97b024cd5
yayan89 <yandatson@gmail.com>
2021-01-08 16:08:31 -0500

    Completed longest peak

--- src/arrays/longestpeak/Main.java
+++ src/arrays/longestpeak/Main.java
@@ -11,6 +11,9 @@ public class Main {
         array = new int[] {1, 3, 2};
         System.out.println(longestPeak(array));
 
+        array = new int[] {1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3};
+        System.out.println(longestPeak(array));
+
         array = new int[] {1, 2, 3, 4, 5, 6, 10, 100, 1000};
         System.out.println(longestPeak(array));
 
@@ -20,7 +23,33 @@ public class Main {
     }
 
     public static int longestPeak(int[] array) {
-        // Write your code here.
-        return -1;
+        int longestPeakLength = 0;
+        int i = 1;
+
+        while (i < array.length - 1) {
+            boolean isPeak = array[i - 1] < array[i] && array[i] > array[i + 1];
+
+            if (!isPeak) {
+                i += 1;
+                continue;
+            }
+
+            int leftIndex = i - 2;
+            while (leftIndex >= 0 && array[leftIndex] < array[leftIndex + 1]) {
+                leftIndex -= 1;
+            }
+
+            int rightIndex = i + 2;
+            while (rightIndex < array.length && array[rightIndex] < array[rightIndex - 1]) {
+                rightIndex += 1;
+            }
+
+            int currentPeakLength = rightIndex - leftIndex - 1;
+            if (currentPeakLength > longestPeakLength) {
+                longestPeakLength = currentPeakLength;
+            }
+            i = rightIndex;
+        }
+        return longestPeakLength;
     }
 }

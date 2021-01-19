5f82492ea66d53bd8b2478428073279895270076
yayan89 <yandatson@gmail.com>
2021-01-19 18:48:49 -0500

    Added InsertionSort

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -5,35 +5,27 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void main(String[] args) {
         int[] array = {20, 35, -15, 7, 55, 1, 22};
-        sort2(array);
+        System.out.println(Arrays.toString(insertionSort(array)));
     }
 
-    private static void sort(int[] array) {
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex <= array.length - 1; firstUnsortedIndex++) {
+    public static int[] insertionSort(int[] array) {
+        if (array.length == 0) {
+            return new int[] {};
+        }
 
-            for (int k = 0; k < firstUnsortedIndex; k++) {
-                if (array[k] > array[firstUnsortedIndex]) {
-                    int holder = array[k];
-                    array[k] = array[firstUnsortedIndex];
-                    array[firstUnsortedIndex] = holder;
-                }
+        for (int i = 1; i < array.length; i++) {
+            int j = i;
+            while (j > 0 && array[j] < array[j - 1]) {
+                swap(j, j - 1, array);
+                j -= 1;
             }
         }
-        System.out.println(Arrays.toString(array));
+        return array;
     }
 
-    private static void sort2(int[] array) {
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex < array.length; firstUnsortedIndex++) {
-            int newElement = array[firstUnsortedIndex];
-            int i;
-
-            for (i = firstUnsortedIndex; i > 0 && array[i - 1] > newElement; i--) {
-                array[i] = array[i - 1];
-            }
-            array[i] = newElement;
-        }
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
+    public static void swap(int i, int j, int[] array) {
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
     }
 }
